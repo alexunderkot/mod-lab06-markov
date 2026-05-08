836c85101fa188a2e29fd7b7d006b6822b089fd0
Александр Арискин <alexunderkot@gmail.com>
2026-05-09 00:09:50 +0300

    4 fix

--- src/textgen.cpp
+++ src/textgen.cpp
@@ -13,8 +13,10 @@ void textgen(int Npref, int MaxGen,
 
     *textout += p.front() + " " + p.back() + " ";
 
+    unsigned int seed = 100;
+
     for (int i = 1; i < MaxGen; i++) {
-        int r = rand_r(100) % statetab[p].size();
+        int r = rand_r(&seed) % statetab[p].size();
         *textout += statetab[p][r] + " " + (i % 4 == 0 ? "\n" : "");
         p.push_back(statetab[p][r]);
         p.pop_front();
