52f010709b1da6d0e3015710f0010b694445cedd
Александр Арискин <alexunderkot@gmail.com>
2026-05-09 00:27:02 +0300

    9 fix

--- test/tests.cpp
+++ test/tests.cpp
@@ -164,7 +164,7 @@ TEST(task4, multiSuffix1) {
 
     string textout = "";
     srand(100);
-    for (int i = 0; i < 100000; i++) {
+    for (int i = 0; i < 100; i++) {
         textgen(2, 2, statetab, &textout);
     }
 
@@ -226,7 +226,7 @@ TEST(task5, textNotEmpty) {
 }
 
 TEST(task5, textLength1) {
-    ifstream file("../../../test/testText2.txt");
+    ifstream file("../../../../test/testText2.txt");
     string word;
     string text;
 
